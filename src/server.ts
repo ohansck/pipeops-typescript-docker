ce25382fdfdcf1aee4445376f8bbf5fce47de8e5
ohansck <ohansck@gmail.com>
2023-10-08 18:51:27 +0100

    kk

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ import bodyParser from 'body-parser';
 
     // Root URI call
     app.get('/', async (req, res) => {
-        res.send({ message: "hello v3", status: "ok" });
+        res.send({ message: "Hello World Ire O", status: "ok" });
     });
 
 
